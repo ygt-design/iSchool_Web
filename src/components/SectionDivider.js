b427949f05ecae6797ec99b3007a8d2800a3c1c9
ygt-design <103063122+ygt-design@users.noreply.github.com>
2024-11-28 13:40:12 -0500

    Updates

--- src/components/SectionDivider.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../styles/SectionDivider.css";
-
-function SectionDivider({ imageClass }) {
-  return <div className={`section-divider ${imageClass}`}></div>;
-}
-
-export default SectionDivider;
